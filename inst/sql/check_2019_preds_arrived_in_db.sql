1379129b93752ce312d06cf8b5aa0d8c77fb0986
Jenkins Daemon <brother-mfc@inwt-statistics.de>
2025-10-28 18:52:09 +0100

    Version: 25.10.2

--- inst/sql/check_2019_preds_arrived_in_db.sql
+++ inst/sql/check_2019_preds_arrived_in_db.sql
@@ -1,2 +1,2 @@
-select count(*) = 24 * 365 * 360765 as all_preds_arrived -- full, cropped Berlin raster has 360765 cells
+select count(*) >= 24 * 365 * 118662 as all_preds_arrived -- full, cropped Berlin raster has 360765 cells
 from traffic_model_predictions_2019;
